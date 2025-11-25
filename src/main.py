e0d19887043f8e0a3d63827b76442f0584e72b3d
João Victor Morais Mendes <joao.mendes.valenca@gmail.com>
2025-11-25 15:42:45 -0300

    fix `ruff check` error

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from .api.v1.processes.router import router as processo_router
 from .api.v1.status.router import router as status_router
 
-app = FastAPI(title="Lysis DB API", version="1.0.0", docs_url="/docs", redoc_url="/redoc")
+app = FastAPI(
+  title="Lysis DB API",
+  version="1.0.0",
+  docs_url="/docs",
+  redoc_url="/redoc"
+)
 
 app.include_router(processo_router)
 app.include_router(status_router)
