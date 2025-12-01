17029964e148da6c83feb74f7ccf53518bdb020a
victor mendes <victormmendes.dev@gmail.com>
2025-12-01 14:45:27 -0300

    add middleware

--- src/main.py
+++ src/main.py
@@ -2,12 +2,25 @@ from fastapi import FastAPI
 
 from .api.v1.processes.router import router as processo_router
 from .api.v1.status.router import router as status_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
   title="Lysis DB API",
   version="1.0.0",
   docs_url="/docs",
   redoc_url="/redoc"
+)
+
+origins = [
+    "http://localhost:4546",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(processo_router)
